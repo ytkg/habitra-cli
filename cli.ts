cc9325042a9ce81c0eb70672d13217ad177d1da9
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-06-21 22:09:46 +0900

    Manage versions in a separate file

--- cli.ts
+++ cli.ts
@@ -1,9 +1,10 @@
 import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
-import { userCommand } from "./command/user.ts"
+import { VERSION } from "./version.ts";
+import { userCommand } from "./command/user.ts";
 
 await new Command()
   .name("habitra-cli")
-  .version("0.0.0")
+  .version(VERSION)
   .description("Command line tool for HabiTra")
   .command("user", userCommand)
   .parse(Deno.args);
