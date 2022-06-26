40f2e9755051739fa7aad33d5a8d7891f8e754c5
m.yuki <matsutani.r.communication@gmail.com>
2022-06-26 18:55:15 +0900

    Add create habit command

--- cli.ts
+++ cli.ts
@@ -1,10 +1,12 @@
 import { Command } from "./deps.ts";
 import { VERSION } from "./version.ts";
 import { userCommand } from "./command/user.ts";
+import { habitCommand } from "./command/habit.ts";
 
 await new Command()
   .name("habitra-cli")
   .version(VERSION)
   .description("Command line tool for HabiTra")
   .command("user", userCommand)
+  .command("habit", habitCommand)
   .parse(Deno.args);
---
+++ command/habit.ts
@@ -0,0 +1,28 @@
+import { Command } from "../deps.ts";
+
+const apiBase = "https://api.habitra.io";
+
+const createCommand = new Command()
+  .description("Create habit command")
+  .option("--id <id:string>", "id", { required: true })
+  .option("--name <name:string>", "name", { required: true })
+  .env("HABITRA_ID=<value:string>", "Habitra ID", { required: true })
+  .env("HABITRA_PASSWORD=<value:string>", "Habitra Password", {
+    required: true,
+  })
+  .action(async (options) => {
+    const response = await fetch(`${apiBase}/v1/users/${options.habitraId}/habits`, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${
+          btoa(`${options.habitraId}:${options.habitraPassword}`)
+        }`,
+      },
+      body: JSON.stringify({ id: options.id, name: options.name }),
+    });
+    console.log(await response.text());
+  });
+
+export const habitCommand = new Command()
+  .description("Habit command")
+  .command("create", createCommand)
