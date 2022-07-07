6e1960ef7ba686f22b152f35461e7f9ca205ba9c
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-07-07 22:51:33 +0900

    Add update habit command

---
+++ commands/habit/update_test.ts
@@ -0,0 +1,29 @@
+import { assertEquals } from "../../dev_deps.ts";
+import { UpdateCommand } from "./update.ts";
+
+const command = new UpdateCommand().help({ colors: false }).helpOption(false);
+
+Deno.test("help command", () => {
+  const output = command.getHelp();
+
+  assertEquals(
+    output,
+    `
+  Usage: COMMAND
+
+  Description:
+
+    Update habit command
+
+  Options:
+
+    --id    <id>    - id    (required)
+    --name  <name>  - name  (required)
+
+  Environment variables:
+
+    HABITRA_ID        <value>  - Habitra ID      
+    HABITRA_PASSWORD  <value>  - Habitra Password
+`,
+  );
+});
