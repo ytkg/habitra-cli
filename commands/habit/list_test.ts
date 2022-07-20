2749fda91fb4e58ae1221d4f2046e11751433b07
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-07-20 22:21:30 +0900

    Add list habit command

---
+++ commands/habit/list_test.ts
@@ -0,0 +1,24 @@
+import { assertEquals } from "../../dev_deps.ts";
+import { ListCommand } from "./list.ts";
+
+const command = new ListCommand().help({ colors: false }).helpOption(false);
+
+Deno.test("help command", () => {
+  const output = command.getHelp();
+
+  assertEquals(
+    output,
+    `
+  Usage: COMMAND
+
+  Description:
+
+    Show habit command
+
+  Environment variables:
+
+    HABITRA_ID        <value>  - Habitra ID      
+    HABITRA_PASSWORD  <value>  - Habitra Password
+`,
+  );
+});
