eb8833ed1cd94047a07261f27648d129ad8137eb
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-10-07 20:17:53 +0900

    Add prefix "v" to verison number

--- cli.ts
+++ cli.ts
@@ -2,5 +2,5 @@ import { MainCommand } from "./commands/main.ts";
 import { VERSION } from "./version.ts";
 
 await new MainCommand()
-  .version(VERSION)
+  .version(`v${VERSION}`)
   .parse(Deno.args);
