092dbdd367fc214d7b344d615b2b9694525a18af
denopendabot <denopendabot@github.com>
2022-12-04 00:19:42 +0000

    build(deps): bump deno.land/x/cliffy from v0.25.4 to v0.25.5

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
   Command,
   HelpCommand,
-} from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+} from "https://deno.land/x/cliffy@v0.25.5/command/mod.ts";
 export {
   DenoLandProvider,
   UpgradeCommand,
-} from "https://deno.land/x/cliffy@v0.25.4/command/upgrade/mod.ts";
+} from "https://deno.land/x/cliffy@v0.25.5/command/upgrade/mod.ts";
